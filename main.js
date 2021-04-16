e8a3d072097537401c5410e3754c912d12cf40df
creatorneemay <72016905+creatorneemay@users.noreply.github.com>
2021-04-16 19:32:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ rwristx="";
 lwristy="";
 rwristy="";
 lwrists="";
+rwrists="";
 function preload(){
     song=loadSound("music.mp3");
 }
@@ -34,19 +35,43 @@ function gotposes(results){
         rwristx=results[0].pose.rightWrist.x;
         rwristy=results[0].pose.rightWrist.y;
         lwrists=results[0].pose.keypoints[9].score;
+        rwrists=results[0].pose.keypoints[10].score;
         console.log(lwristx,lwristy,rwristx,rwristy);
     }
 }
 function draw(){
     image(video,0,0,400,400);
-    if(lwristx>0.2){
-        fill("red");
-        stroke("blue");
+    fill("red");
+    stroke("blue");
+    if(lwrists>0.2){
         circle(lwristx,lwristy,20);
         lwristy_number=Number(lwristy);
         remd=floor(lwristy);
         volume=remd/500;
         song.setVolume(volume);
         document.getElementById("vol").innerHTML="volume= "+ volume;
+    }
+    if(rwrists>0.2){
+        circle(rwristx,rwristy,20);
+        if(rwristy>0&&rwristy<=100){
+            song.rate(0.5)
+            document.getElementById("speed").innerHTML="speed= 0.5x";
+        }
+        else if(rwristy>100&&rwristy<=200){
+            song.rate(1)
+            document.getElementById("speed").innerHTML="speed= 1x";
+        }
+        else if(rwristy>200&&rwristy<=300){
+            song.rate(1.5)
+            document.getElementById("speed").innerHTML="speed= 1.5x";
+        }
+        else if(rwristy>300&&rwristy<=400){
+            song.rate(2)
+            document.getElementById("speed").innerHTML="speed= 2x";
+        }
+        else if(rwristy>400&&rwristy<=500){
+            song.rate(2.5)
+            document.getElementById("speed").innerHTML="speed= 2.5x";
+        }
     }
 }
